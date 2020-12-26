040c187765d51469da35a648de0d3354a9fbb555
abmsonic <haribothehedgehog@gmail.com>
2020-12-26 16:35:06 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true 
+  validates :password, length: { minimum: 6 }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: ' Full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers'
 end
