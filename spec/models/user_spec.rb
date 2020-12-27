4cfe9c131bfc3456867af856e967cb6dd68354b8
abmsonic <haribothehedgehog@gmail.com>
2020-12-27 11:15:48 +0900

    テストコードの追記と編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,27 +10,6 @@ RSpec.describe User, type: :model do
       it 'nicknameとemail、passwordとpassword_confirmation、last_nameとfirst_name、last_name_kanaとfirst_nama_kana、birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
-      it 'passwordとpassword_confirmationが6文字以上かつ半角英数字混合であれば登録できる' do
-        @user.password = 'abc123'
-        @user.password_confirmation = 'abc123'
-        expect(@user).to be_valid
-      end
-      it 'last_nameが全角(漢字、ひらがな、カタカナ)であれば登録できる' do
-        @user.last_name = '佐藤'
-        expect(@user).to be_valid
-      end
-      it 'first_nameが全角(漢字、ひらがな、カタカナ)であれば登録できる' do
-        @user.first_name = '次郎'
-        expect(@user).to be_valid
-      end
-      it 'last_name_kanaが全角(カタカナ)であれば登録できる' do
-        @user.last_name_kana = 'サトウ'
-        expect(@user).to be_valid
-      end
-      it 'first_name_kanaが全角(カタカナ)であれば登録できる' do
-        @user.first_name_kana = 'ジロウ'
-        expect(@user).to be_valid
-      end
     end
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
@@ -85,6 +64,11 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
+      it 'emailに@が含まれていないと登録できない' do
+        @user.email = 'hogegmail.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it 'passwordが5文字以下では登録できない' do
         @user.password = 'aaaa1'
         @user.password_confirmation = 'aaaa1'
@@ -96,6 +80,12 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password include both letters and numbers')
+      end
+      it 'passwordとpassword_confirmationは値が同じでないと登録出来ない' do
+        @user.password = 'aaaaa1'
+        @user.password_confirmation = 'aaaaa2'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'last_nameが全角(漢字、ひらがな、カタカナ)でなければ登録できない' do
         @user.last_name = 'aa'
