558d5f64401f2866b46fd4efeebb6fe1d44f668b
abmsonic <haribothehedgehog@gmail.com>
2021-01-05 21:49:21 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
@@ -9,6 +10,7 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name
+    validates :image
     validates :info
     validates :category_id
     validates :status_id
@@ -17,8 +19,10 @@ class Item < ApplicationRecord
     validates :user_id
   end
 
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number'}
   validates :price, numericality: { greater_than_or_equal_to: 300,
-     less_than_or_equal_to: 9999999
+     less_than_or_equal_to: 9999999,
+     message: 'Out of setting range'
      }
 
   validates :category_id, numericality: { other_than: 1 }
--- db/migrate/20210105050644_create_items.rb
+++ db/migrate/20210105050644_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer    :category_id  , null: false
       t.integer    :status_id    , null: false
       t.integer    :ship_fee_id  , null: false
-      t.integer    :prefecure_id , null: false
+      t.integer    :prefecture_id , null: false
       t.integer    :ship_day_id  , null: false
       t.integer    :price        , null: false
       t.references :user         , foregin_key: true
