a793d3f2d1e11ea7e6821a1279167aec8b9ec93e
abmsonic <haribothehedgehog@gmail.com>
2021-01-05 21:50:49 +0900

    コントローラーとビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :move_to_session, except: :index
+
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+      params.require(:item).permit(:name, :image, :info, :category_id, :status_id, :ship_fee_id, :prefecture_id, :ship_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
