2ac9bfd8a734dc8d36151dd5e73b660e2e2ddd73
abmsonic <haribothehedgehog@gmail.com>
2021-01-08 12:04:59 +0900

    修正01

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,14 +20,16 @@ class Item < ApplicationRecord
     validates :price
   end
 
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :status_id
+    validates :ship_fee_id
+    validates :prefecture_id
+    validates :ship_day_id
+  end
+
   validates :price, numericality: { only_integer: true, message: 'Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300,
                                     less_than_or_equal_to: 9_999_999,
                                     message: 'Out of setting range' }
-
-  validates :category_id, numericality: { other_than: 1, message: 'Select' }
-  validates :status_id, numericality: { other_than: 1, message: 'Select' }
-  validates :ship_fee_id, numericality: { other_than: 1, message: 'Select' }
-  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
-  validates :ship_day_id, numericality: { other_than: 1, message: 'Select' }
 end
