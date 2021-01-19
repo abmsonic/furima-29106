6a77d57d57d333739aa6d341b3d1aa84dbb85fe2
abmsonic <haribothehedgehog@gmail.com>
2021-01-19 16:07:09 +0900

    フォームオブジェクトの実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+
+end
